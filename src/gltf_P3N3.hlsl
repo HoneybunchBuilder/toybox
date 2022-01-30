2022fb609696b105c5f4cb4b0a7e7fc6bd9d7428
Honeybunch <amt3824@g.rit.edu>
2022-01-30 13:31:53 -0800

    Fixing up some shaders

--- src/gltf_P3N3.hlsl
+++ src/gltf_P3N3.hlsl
@@ -43,39 +43,53 @@ Interpolators vert(VertexIn i)
 
 float4 frag(Interpolators i) : SV_TARGET
 {
-    // TODO: Get material albedo color some other way
-    float3 albedo = float3(0.5, 0.5, 0.5);
+    // TODO: Get material base color color some other way
+    float3 base_color = float3(0.5, 0.5, 0.5);
 
     float3 N = normalize(i.normal);
 
-    float3 L = normalize(light_data.light_dir);
     float3 V = normalize(camera_data.view_pos - i.world_pos);
-    float3 H = normalize(V + L);
+    float NdotV = clamp(abs(dot(N, V)), 0.001, 1.0);
 
-    float3 color = float3(0.0, 0.0, 0.0);
+    float3 out_color = float3(0.0, 0.0, 0.0);
 
     if(PermutationFlags & GLTF_PERM_PBR_METALLIC_ROUGHNESS)
     {
         float metallic = material_data.pbr_metallic_roughness.metallic_factor;
         float roughness = material_data.pbr_metallic_roughness.roughness_factor;
 
-        float3 F0 = float3(0.04, 0.04, 0.04);
-        F0 = lerp(F0, albedo, metallic);
+        float alpha_roughness = roughness * roughness;
 
-        // Reflectance
-        float3 Lo = float3(0.0, 0.0, 0.0);
+        float3 f0 = float3(0.4, 0.4, 0.4);
+
+        float3 diffuse_color = base_color * (float3(1.0, 1.0, 1.0) - f0);
+        diffuse_color *= 1.0 - metallic;
+
+        float3 specular_color = lerp(f0, base_color, metallic);
+        float reflectance = max(max(specular_color.r, specular_color.g), specular_color.b);
+
+        // For typical incident reflectance range (between 4% to 100%) set the grazing reflectance to 100% for typical fresnel effect.
+        // For very low reflectance range on highly diffuse objects (below 4%), incrementally reduce grazing reflecance to 0%.
+        float reflectance_90 = clamp(reflectance * 25.0, 0.0, 1.0);
+        float3 specular_environment_R0 = specular_color;
+        float3 specular_environment_R90 = float3(1.0, 1.0, 1.0) * reflectance_90;
+
         //for each light
         {
             float3 light_color = float3(1, 1, 1);
+            float3 L = normalize(light_data.light_dir);
 
-            Lo += pbr_metal_rough_light(F0, albedo, light_color, metallic, roughness, N, L, V, H);
+            PBRLight light = {
+                light_color,
+                L,
+                specular_environment_R0,
+                specular_environment_R90,
+                alpha_roughness,
+                diffuse_color,
+            };
+
+            out_color += pbr_lighting(light, N, V, NdotV);
         }
-
-        float3 ambient = float3(0.03, 0.03, 0.03) * albedo;
-        color = ambient + Lo;
-        
-        color = color / (color + float3(1.0, 1.0, 1.0));
-        color = pow(color, float3(1.0/2.2, 1.0/2.2, 1.0/2.2));
     }
     else // Phong fallback
     {
@@ -83,10 +97,20 @@ float4 frag(Interpolators i) : SV_TARGET
 
         // for each light
         {
+            float3 L = normalize(light_data.light_dir);
+            float3 H = normalize(V + L);
+
             float3 light_color = float3(1, 1, 1);
-            color += phong_light(albedo, light_color, gloss, N, L, V, H);
+            out_color += phong_light(base_color, light_color, gloss, N, L, V, H);
         }
     }
 
-    return float4(color, 1.0);
+    // Gamma correct
+    out_color = pow(out_color, float3(0.4545, 0.4545, 0.4545));
+
+    // Ambient
+    float3 ambient = float3(AMBIENT, AMBIENT, AMBIENT) * base_color;
+    out_color += ambient;
+
+    return float4(out_color, 1.0);
 }
