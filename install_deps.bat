56716d57d4e4eefffae33777f680c51f5663ff8b
Honeybunch <amt3824@g.rit.edu>
2022-05-21 12:36:30 -0700

    Adding recurse to vcpkg install scripts

--- install_deps.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-call vcpkg install meshoptimizer:x64-windows cgltf:x64-windows imgui:x64-windows ktx[vulkan,tools]:x64-windows mimalloc:x64-windows sdl2[vulkan]:x64-windows sdl2-image:x64-windows volk:x64-windows vulkan-memory-allocator:x64-windows tracy:x64-windows
-
-REM Install Windows Static Deps
-call vcpkg install  meshoptimizer:x64-windows-static cgltf:x64-windows-static imgui:x64-windows-static ktx[vulkan,tools]:x64-windows-static mimalloc:x64-windows-static sdl2[vulkan]:x64-windows-static sdl2-image:x64-windows-static volk:x64-windows-static vulkan-memory-allocator:x64-windows-static tracy:x64-windows-static
--- install_deps_android.bat
+++ install_deps_android.bat
@@ -1,10 +1,10 @@
 @echo off
 
 REM Install Windows Host Tools
-call vcpkg install ktx[vulkan,tools]:x64-windows
+call vcpkg install ktx[vulkan,tools]:x64-windows --recurse
 
 REM Install arm64 Android Deps
-call vcpkg install meshoptimizer:arm64-android cgltf:arm64-android imgui:arm64-android vulkan:arm64-android vulkan-headers:arm64-android ktx[vulkan]:arm64-android mimalloc:arm64-android sdl2[vulkan]:arm64-android sdl2-image:arm64-android volk:arm64-android vulkan-memory-allocator:arm64-android tracy:arm64-android
+call vcpkg install meshoptimizer:arm64-android cgltf:arm64-android imgui:arm64-android vulkan:arm64-android vulkan-headers:arm64-android ktx[vulkan]:arm64-android mimalloc:arm64-android sdl2[vulkan]:arm64-android sdl2-image:arm64-android volk:arm64-android vulkan-memory-allocator:arm64-android tracy:arm64-android --recurse
 
 REM Install x64 Android Deps
-call vcpkg install meshoptimizer:x64-android cgltf:x64-android imgui:x64-android vulkan:x64-android vulkan-headers:x64-android ktx[vulkan]:x64-android mimalloc:x64-android sdl2[vulkan]:x64-android sdl2-image:x64-android volk:x64-android vulkan-memory-allocator:x64-android tracy:x64-android
+call vcpkg install meshoptimizer:x64-android cgltf:x64-android imgui:x64-android vulkan:x64-android vulkan-headers:x64-android ktx[vulkan]:x64-android mimalloc:x64-android sdl2[vulkan]:x64-android sdl2-image:x64-android volk:x64-android vulkan-memory-allocator:x64-android tracy:x64-android --recurse
