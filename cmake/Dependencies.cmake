c8f7f1da4746d0d083c7361d9be578ff34f1c2b7
Honeybunch <amt3824@g.rit.edu>
2022-06-05 17:44:01 -0700

    New dependencies script

---
+++ cmake/Dependencies.cmake
@@ -0,0 +1,58 @@
+
+find_program(VCPKG vcpkg REQUIRED)
+
+set(target ${CMAKE_ARGV3})
+set(linkage ${CMAKE_ARGV4})
+
+if(NOT target)
+    message(FATAL_ERROR "Target not specified")
+endif()
+
+# Android builds are always static
+if(target MATCHES "android")
+    set(linkage "static")
+elseif(NOT linkage)
+    message(FATAL_ERROR "Linkage not specified")
+endif()
+
+set(triplet ${target}-${linkage})
+if(${target} MATCHES "x64-windows" AND ${linkage} MATCHES "dynamic")
+    set(triplet ${target})
+elseif(${target} MATCHES "android")
+    set(triplet ${target})
+endif()
+
+cmake_host_system_information(RESULT host_plat QUERY OS_PLATFORM)
+if(host_plat MATCHES "AMD64")
+    set(host_arch "x64")
+elseif(host_plat MATCHES "arm")
+    set(host_arch "arm64")
+else()
+    message(FATAL_ERROR "Unexpected host platform: ${host_plat}")
+endif()
+
+if(CMAKE_HOST_WIN32)
+    set(host-triplet "${host_arch}-windows")
+elseif(CMAKE_HOST_APPLE)
+    set(host-triplet "${host_arch}-osx")
+elseif(CMAKE_HOST_UNIX)
+    set(host-triplet "${host_arch}-linux")
+else()
+    message(FATAL_ERROR "Unexpected host")
+endif()
+
+set(host_packages "ktx[vulkan,tools]")
+set(packages "meshoptimizer;cgltf;imgui;ktx[vulkan];mimalloc;sdl2[vulkan];sdl2-image;volk;vulkan-memory-allocator")
+
+# Determine host packages
+foreach(package ${host_packages})
+    list(APPEND host_install_list "${package}:${host-triplet}")
+endforeach()
+
+# Determine runtime packages
+foreach(package ${packages})
+    list(APPEND install_list "${package}:${triplet}")
+endforeach()
+
+# Install packages
+execute_process(COMMAND ${VCPKG} install ${install_list})
