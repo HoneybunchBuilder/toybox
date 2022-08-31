7fc900bc293d0e4e05cac04886970f5337dcef60
Honeybunch <amt3824@g.rit.edu>
2022-08-30 17:39:15 -0700

    Setup basic input system flow

--- src/coreuisystem.c
+++ src/coreuisystem.c
@@ -18,14 +18,95 @@ bool create_coreui_system(CoreUISystem *self,
 
 void destroy_coreui_system(CoreUISystem *self) { *self = (CoreUISystem){0}; }
 
+void coreui_show_about(bool *open) {
+  if (igBegin("About Toybox", open, 0)) {
+    igEnd();
+  }
+}
+
 void tick_coreui_system(CoreUISystem *self, const SystemInput *input,
                         SystemOutput *output, float delta_seconds) {
-  (void)self;
-  (void)input;
-  (void)output;
   (void)delta_seconds;
   TracyCZoneN(ctx, "Core UI System Tick", true);
   TracyCZoneColor(ctx, TracyCategoryColorUI);
+
+  // Find expected components
+  uint32_t entity_count = 0;
+  const EntityId *entities = NULL;
+  const PackedComponentStore *coreui_comp_store = NULL;
+  const PackedComponentStore *imgui_comp_store = NULL;
+  TB_CHECK(input->dep_set_count == 1, "Unexpected number of dependency sets");
+  for (uint32_t dep_set_idx = 0; dep_set_idx < input->dep_set_count;
+       ++dep_set_idx) {
+    const SystemDependencySet *dep_set = &input->dep_sets[dep_set_idx];
+    entities = dep_set->entity_ids;
+    entity_count = dep_set->entity_count;
+    for (uint32_t col_idx = 0; col_idx < dep_set->column_count; ++col_idx) {
+      const PackedComponentStore *column = &dep_set->columns[col_idx];
+      if (column->id == CoreUIComponentId) {
+        coreui_comp_store = column;
+      }
+      if (column->id == ImGuiComponentId) {
+        imgui_comp_store = column;
+      }
+    }
+  }
+  if (entity_count > 0) {
+    TB_CHECK(entities, "Invalid input entities");
+    TB_CHECK(coreui_comp_store, "Failed to find coreui component store");
+    TB_CHECK(imgui_comp_store, "Failed to find imgui component store");
+
+    CoreUIComponent *out_coreui =
+        tb_alloc_nm_tp(self->tmp_alloc, entity_count, CoreUIComponent);
+    SDL_memset(out_coreui, 0, sizeof(CoreUIComponent) * entity_count);
+
+    for (uint32_t entity_idx = 0; entity_idx < entity_count; ++entity_idx) {
+      const CoreUIComponent *coreui =
+          &((const CoreUIComponent *)coreui_comp_store->components)[entity_idx];
+      const ImGuiComponent *imgui =
+          &((const ImGuiComponent *)imgui_comp_store->components)[entity_idx];
+
+      *out_coreui = *coreui;
+
+      igSetCurrentContext(imgui->context);
+
+      if (igBeginMainMenuBar()) {
+        if (igBeginMenu("About", true)) {
+          out_coreui->show_about = !out_coreui->show_about;
+          igEndMenu();
+        }
+        if (igBeginMenu("Demo", true)) {
+          out_coreui->show_demo = !out_coreui->show_demo;
+          igEndMenu();
+        }
+        if (igBeginMenu("Metrics", true)) {
+          out_coreui->show_metrics = !out_coreui->show_metrics;
+          igEndMenu();
+        }
+        igEndMainMenuBar();
+      }
+
+      if (out_coreui->show_all) {
+        if (out_coreui->show_about) {
+          coreui_show_about(&out_coreui->show_about);
+        }
+        if (out_coreui->show_demo) {
+          igShowDemoWindow(&out_coreui->show_demo);
+        }
+        if (out_coreui->show_metrics) {
+          igShowMetricsWindow(&out_coreui->show_metrics);
+        }
+      }
+    }
+
+    output->set_count = 1;
+    output->write_sets[0] = (SystemWriteSet){
+        .id = CoreUIComponentId,
+        .count = entity_count,
+        .entities = entities,
+        .components = (uint8_t *)out_coreui,
+    };
+  }
 
   TracyCZoneEnd(ctx);
 }
@@ -40,7 +121,7 @@ void tb_coreui_system_descriptor(SystemDescriptor *desc,
   desc->desc = (InternalDescriptor)coreui_desc;
   SDL_memset(desc->deps, 0,
              sizeof(SystemComponentDependencies) * MAX_DEPENDENCY_SET_COUT);
-  desc->dep_count = 2;
+  desc->dep_count = 1;
   desc->deps[0] = (SystemComponentDependencies){
       .count = 2,
       .dependent_ids = {CoreUIComponentId, ImGuiComponentId},
--- src/imguisystem.c
+++ src/imguisystem.c
@@ -4,6 +4,7 @@
 #include "inputcomponent.h"
 #include "profiling.h"
 #include "tbcommon.h"
+#include "tbimgui.h"
 
 bool create_imgui_system(ImGuiSystem *self, const ImGuiSystemDescriptor *desc) {
   TB_CHECK_RETURN(desc, "Invalid descriptor", false);
@@ -20,11 +21,77 @@ void destroy_imgui_system(ImGuiSystem *self) { *self = (ImGuiSystem){0}; }
 void tick_imgui_system(ImGuiSystem *self, const SystemInput *input,
                        SystemOutput *output, float delta_seconds) {
   (void)self;
-  (void)input;
   (void)output; // No output for this system
   (void)delta_seconds;
   TracyCZoneN(ctx, "ImGui System", true);
   TracyCZoneColor(ctx, TracyCategoryColorUI);
+
+  // Find expected components
+  uint32_t imgui_entity_count = 0;
+  const EntityId *imgui_entities = NULL;
+  const PackedComponentStore *imgui_comp_store = NULL;
+
+  uint32_t input_entity_count = 0;
+  const PackedComponentStore *input_comp_store = NULL;
+  TB_CHECK(input->dep_set_count == 2, "Unexpected number of dependency sets");
+  for (uint32_t dep_set_idx = 0; dep_set_idx < input->dep_set_count;
+       ++dep_set_idx) {
+    const SystemDependencySet *dep_set = &input->dep_sets[dep_set_idx];
+
+    for (uint32_t col_idx = 0; col_idx < dep_set->column_count; ++col_idx) {
+      const PackedComponentStore *column = &dep_set->columns[col_idx];
+      if (column->id == ImGuiComponentId) {
+        imgui_comp_store = column;
+        imgui_entities = dep_set->entity_ids;
+        imgui_entity_count = dep_set->entity_count;
+      }
+
+      if (column->id == InputComponentId) {
+        input_comp_store = column;
+        input_entity_count = dep_set->entity_count;
+      }
+    }
+  }
+  if (imgui_entity_count > 0) {
+    TB_CHECK(imgui_entities, "Invalid input entities");
+    TB_CHECK(imgui_comp_store, "Failed to find imgui component store");
+
+    for (uint32_t entity_idx = 0; entity_idx < imgui_entity_count;
+         ++entity_idx) {
+      const ImGuiComponent *imgui =
+          &((const ImGuiComponent *)imgui_comp_store->components)[entity_idx];
+
+      igSetCurrentContext(imgui->context);
+
+      // Apply this frame's input
+      for (uint32_t input_idx = 0; input_idx < input_entity_count;
+           ++input_idx) {
+        const InputComponent *input =
+            &((const InputComponent *)input_comp_store->components)[input_idx];
+        for (uint32_t event_idx = 0; event_idx < input->event_count;
+             ++event_idx) {
+          const SDL_Event *event = &input->events[event_idx];
+
+          // TODO: Feed event to imgui
+          (void)event;
+        }
+      }
+
+      // Apply basic IO
+      ImGuiIO *io = igGetIO();
+      io->DeltaTime = delta_seconds; // Note that ImGui expects seconds
+      // TODO: Fetch this from the renderer
+      io->DisplaySize = (ImVec2){1600.0f, 900.0f};
+
+      igRender();
+
+      ImDrawData *draw_data = igGetDrawData();
+      (void)draw_data;
+      TB_CHECK(draw_data, "Failed to retrieve draw data");
+
+      igNewFrame();
+    }
+  }
 
   TracyCZoneEnd(ctx);
 }
@@ -39,7 +106,7 @@ void tb_imgui_system_descriptor(SystemDescriptor *desc,
   desc->desc = (InternalDescriptor)input_desc;
   SDL_memset(desc->deps, 0,
              sizeof(SystemComponentDependencies) * MAX_DEPENDENCY_SET_COUT);
-  desc->dep_count = 1;
+  desc->dep_count = 2;
   desc->deps[0] = (SystemComponentDependencies){
       .count = 1,
       .dependent_ids = {InputComponentId},
