a81d29d61707071727cc252257cd9c2271fbde23
Honeybunch <amt3824@g.rit.edu>
2022-09-11 16:38:41 -0700

    More scaffolding

--- src/meshcomponent.h
+++ src/meshcomponent.h
@@ -1,4 +1,24 @@
 #pragma once
 
+#include "tbrendercommon.h"
+
 #define MeshComponentId 0x0D15EA5E
 #define MeshComponentIdStr "0x0D15EA5E"
+
+typedef struct ComponentDescriptor ComponentDescriptor;
+
+typedef struct cgltf_mesh cgltf_mesh;
+
+typedef struct MeshComponentDescriptor {
+  const char *source_path;
+  const cgltf_mesh *mesh;
+} MeshComponentDescriptor;
+
+typedef struct MeshComponent {
+  TbBuffer geom_buffer;
+  uint32_t index_count;
+  uint32_t index_offset;
+  uint32_t vertex_offset;
+} MeshComponent;
+
+void tb_mesh_component_descriptor(ComponentDescriptor *desc);
--- src/meshsystem.h
+++ src/meshsystem.h
@@ -8,6 +8,10 @@
 
 typedef struct SystemDescriptor SystemDescriptor;
 typedef struct RenderSystem RenderSystem;
+typedef struct cgltf_mesh cgltf_mesh;
+typedef struct VkBuffer_T *VkBuffer;
+
+typedef uint32_t TbMeshId;
 
 typedef struct MeshSystemDescriptor {
   Allocator std_alloc;
@@ -23,11 +27,23 @@ typedef struct MeshSystem {
   VkRenderPass pass;
   VkFramebuffer framebuffers[TB_MAX_FRAME_STATES];
 
-  VkSampler sampler;
   VkDescriptorSetLayout set_layout;
   VkPipelineLayout pipe_layout;
   VkPipeline pipeline;
+
+  uint32_t mesh_count;
+  TbMeshId *mesh_ids;
+  TbBuffer *mesh_host_buffers;
+  TbBuffer *mesh_gpu_buffers;
+  uint32_t *mesh_ref_counts;
+  uint32_t mesh_max;
 } MeshSystem;
 
 void tb_mesh_system_descriptor(SystemDescriptor *desc,
                                const MeshSystemDescriptor *mesh_desc);
+
+bool tb_mesh_system_load_mesh(MeshSystem *self, const char *path,
+                              const cgltf_mesh *mesh, TbMeshId *id);
+bool tb_mesh_system_take_mesh_ref(MeshSystem *self, TbMeshId id);
+VkBuffer tb_mesh_system_get_gpu_mesh(MeshSystem *self, TbMeshId id);
+void tb_mesh_system_release_mesh_ref(MeshSystem *self, TbMeshId id);
