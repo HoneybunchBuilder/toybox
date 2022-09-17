270feb68625757a180733b4f89fe599d7dec3906
Honeybunch <amt3824@g.rit.edu>
2022-09-17 15:20:09 -0700

    Fixing up input

--- src/noclipcontrollersystem.c
+++ src/noclipcontrollersystem.c
@@ -59,8 +59,11 @@ void tick_noclip_system(NoClipControllerSystem *self, const SystemInput *input,
     if (transform_store != NULL && noclip_store != NULL &&
         input_store != NULL) {
 
-      const NoClipComponent *noclip_comps =
-          (const NoClipComponent *)noclip_store->components;
+      // Copy the noclip component for output
+      NoClipComponent *out_noclips =
+          tb_alloc_nm_tp(self->tmp_alloc, entity_count, NoClipComponent);
+      SDL_memcpy(out_noclips, noclip_store->components,
+                 entity_count * sizeof(NoClipComponent));
 
       // Make a copy of the transform input as the output
       TransformComponent *out_transforms =
@@ -68,129 +71,130 @@ void tick_noclip_system(NoClipControllerSystem *self, const SystemInput *input,
       SDL_memcpy(out_transforms, transform_store->components,
                  entity_count * sizeof(TransformComponent));
 
-      // Based on the input, modify all the transform components for each
-      // entity
-      const InputComponent *input_comps =
-          (const InputComponent *)input_store->components;
-      for (uint32_t in_idx = 0; in_idx < input_comp_count; ++in_idx) {
-        const InputComponent *input_comp = &input_comps[in_idx];
+      for (uint32_t trans_idx = 0; trans_idx < entity_count; ++trans_idx) {
+        NoClipComponent *noclip = &out_noclips[trans_idx];
+        TransformComponent *transform = &out_transforms[trans_idx];
 
-        for (uint32_t event_idx = 0; event_idx < input_comp->event_count;
-             ++event_idx) {
-          const SDL_Event *event = &input_comp->events[event_idx];
-          const uint32_t event_type = event->type;
+        NoClipState state = noclip->state;
+        int32_t mouse_x_delta = 0;
+        int32_t mouse_y_delta = 0;
 
-          NoClipState state = 0;
-          int32_t mouse_x_delta = 0;
-          int32_t mouse_y_delta = 0;
+        // Based on the input, modify all the transform components for each
+        // entity
+        const InputComponent *input_comps =
+            (const InputComponent *)input_store->components;
+        for (uint32_t in_idx = 0; in_idx < input_comp_count; ++in_idx) {
+          const InputComponent *input_comp = &input_comps[in_idx];
 
-          switch (event_type) {
-          case SDL_KEYDOWN:
-          case SDL_KEYUP: {
-            NoClipState edit_state = 0;
-            const SDL_Keysym *keysym = &event->key.keysym;
-            SDL_Scancode scancode = keysym->scancode;
+          for (uint32_t event_idx = 0; event_idx < input_comp->event_count;
+               ++event_idx) {
+            const SDL_Event *event = &input_comp->events[event_idx];
+            const uint32_t event_type = event->type;
 
-            if (scancode == SDL_SCANCODE_W) {
-              edit_state = NOCLIP_MOVING_FORWARD;
-            } else if (scancode == SDL_SCANCODE_A) {
-              edit_state = NOCLIP_MOVING_LEFT;
-            } else if (scancode == SDL_SCANCODE_S) {
-              edit_state = NOCLIP_MOVING_BACKWARD;
-            } else if (scancode == SDL_SCANCODE_D) {
-              edit_state = NOCLIP_MOVING_RIGHT;
+            switch (event_type) {
+            case SDL_KEYDOWN:
+            case SDL_KEYUP: {
+              NoClipState edit_state = 0;
+              const SDL_Keysym *keysym = &event->key.keysym;
+              SDL_Scancode scancode = keysym->scancode;
+
+              if (scancode == SDL_SCANCODE_W) {
+                edit_state = NOCLIP_MOVING_FORWARD;
+              } else if (scancode == SDL_SCANCODE_A) {
+                edit_state = NOCLIP_MOVING_LEFT;
+              } else if (scancode == SDL_SCANCODE_S) {
+                edit_state = NOCLIP_MOVING_BACKWARD;
+              } else if (scancode == SDL_SCANCODE_D) {
+                edit_state = NOCLIP_MOVING_RIGHT;
+              }
+
+              if (event_type == SDL_KEYDOWN) {
+                state |= edit_state;
+              } else if (event_type == SDL_KEYUP) {
+                state &= ~edit_state;
+              }
+              break;
             }
+            case SDL_MOUSEMOTION: {
+              const SDL_MouseMotionEvent *mouse_motion = &event->motion;
+              // What buttons are down while the mouse is moving?
+              uint32_t button_state = mouse_motion->state;
 
-            if (event_type == SDL_KEYDOWN) {
-              state |= edit_state;
-            } else if (event_type == SDL_KEYUP) {
-              state &= ~edit_state;
+              // if SOME button is pressed
+              if (button_state != 0) {
+                mouse_x_delta += mouse_motion->xrel;
+                mouse_y_delta += mouse_motion->yrel;
+
+                if (mouse_x_delta > 0) {
+                  state |= NOCLIP_LOOKING_RIGHT;
+                } else if (mouse_x_delta < 0) {
+                  state |= NOCLIP_LOOKING_RIGHT;
+                }
+                if (mouse_y_delta > 0) {
+                  state |= NOCLIP_LOOKING_DOWN;
+                } else if (mouse_y_delta < 0) {
+                  state |= NOCLIP_LOOKING_UP;
+                }
+              }
+
+              break;
             }
-            break;
-          }
-          case SDL_MOUSEMOTION: {
-            const SDL_MouseMotionEvent *mouse_motion = &event->motion;
-            // What buttons are down while the mouse is moving?
-            uint32_t button_state = mouse_motion->state;
-
-            // if SOME button is pressed
-            if (button_state != 0) {
-              mouse_x_delta += mouse_motion->xrel;
-              mouse_y_delta += mouse_motion->yrel;
-
-              if (mouse_x_delta > 0) {
-                state |= NOCLIP_LOOKING_RIGHT;
-              } else if (mouse_x_delta < 0) {
-                state |= NOCLIP_LOOKING_RIGHT;
-              }
-              if (mouse_y_delta > 0) {
-                state |= NOCLIP_LOOKING_DOWN;
-              } else if (mouse_y_delta < 0) {
-                state |= NOCLIP_LOOKING_UP;
-              }
-            }
-
-            break;
-          }
-          default:
-            break;
-          }
-
-          // If we have a state to process, transform each output
-          // transform appropriately
-          if (state) {
-            for (uint32_t trans_idx = 0; trans_idx < entity_count;
-                 ++trans_idx) {
-              const NoClipComponent *noclip = &noclip_comps[trans_idx];
-              TransformComponent *out_transform = &out_transforms[trans_idx];
-
-              float4x4 mat = {.row0 = {0}};
-              transform_to_matrix(&mat, &out_transform->transform);
-
-              float3 right = f4tof3(mat.row0);
-              float3 forward = f4tof3(mat.row2);
-
-              float3 velocity = {0};
-              {
-                float delta_move_speed = noclip->move_speed * delta_seconds;
-                if (state & NOCLIP_MOVING_FORWARD) {
-                  velocity -= forward * delta_move_speed;
-                }
-                if (state & NOCLIP_MOVING_LEFT) {
-                  velocity -= right * delta_move_speed;
-                }
-                if (state & NOCLIP_MOVING_BACKWARD) {
-                  velocity += forward * delta_move_speed;
-                }
-                if (state & NOCLIP_MOVING_RIGHT) {
-                  velocity += right * delta_move_speed;
-                }
-              }
-
-              float3 angular_velocity = {0};
-              if (state & NOCLIP_LOOKING) {
-                float delta_look_speed = noclip->look_speed * delta_seconds;
-                if (state & NOCLIP_LOOKING_RIGHT ||
-                    state & NOCLIP_LOOKING_LEFT) {
-                  angular_velocity[1] +=
-                      (float)mouse_x_delta * delta_look_speed;
-                }
-                if (state & NOCLIP_LOOKING_DOWN || state & NOCLIP_LOOKING_UP) {
-                  angular_velocity[0] +=
-                      (float)mouse_y_delta * delta_look_speed;
-                }
-              }
-
-              out_transform->transform.position += velocity;
-              out_transform->transform.rotation += angular_velocity;
+            default:
+              break;
             }
           }
         }
+
+        // Modify transform based on state
+        float4x4 mat = {.row0 = {0}};
+        transform_to_matrix(&mat, &transform->transform);
+
+        float3 right = f4tof3(mat.row0);
+        float3 forward = f4tof3(mat.row2);
+
+        float3 velocity = {0};
+        {
+          float delta_move_speed = noclip->move_speed * delta_seconds;
+          if (state & NOCLIP_MOVING_FORWARD) {
+            velocity -= forward * delta_move_speed;
+          }
+          if (state & NOCLIP_MOVING_LEFT) {
+            velocity -= right * delta_move_speed;
+          }
+          if (state & NOCLIP_MOVING_BACKWARD) {
+            velocity += forward * delta_move_speed;
+          }
+          if (state & NOCLIP_MOVING_RIGHT) {
+            velocity += right * delta_move_speed;
+          }
+        }
+
+        float3 angular_velocity = {0};
+        if (state & NOCLIP_LOOKING) {
+          float delta_look_speed = noclip->look_speed * delta_seconds;
+          if (state & NOCLIP_LOOKING_RIGHT || state & NOCLIP_LOOKING_LEFT) {
+            angular_velocity[1] += (float)mouse_x_delta * delta_look_speed;
+          }
+          if (state & NOCLIP_LOOKING_DOWN || state & NOCLIP_LOOKING_UP) {
+            angular_velocity[0] += (float)mouse_y_delta * delta_look_speed;
+          }
+        }
+
+        noclip->state = state;
+
+        transform->transform.position += velocity;
+        transform->transform.rotation += angular_velocity;
       }
 
       // Report output
-      output->set_count = 1;
+      output->set_count = 2;
       output->write_sets[0] = (SystemWriteSet){
+          .id = NoClipComponentId,
+          .count = entity_count,
+          .components = (uint8_t *)out_noclips,
+          .entities = entities,
+      };
+      output->write_sets[1] = (SystemWriteSet){
           .id = TransformComponentId,
           .count = entity_count,
           .components = (uint8_t *)out_transforms,
