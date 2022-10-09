c77c3b2c243886fb52024afd6c4eb23a84eb8cfc
Honeybunch <amt3824@g.rit.edu>
2022-10-09 16:25:26 -0700

    Implementing very simple image transitions for passes

--- src/tbrendercommon.h
+++ src/tbrendercommon.h
@@ -51,8 +51,5 @@ typedef struct TbImage {
   VmaAllocationInfo info;
 } TbImage;
 
-typedef void tb_pass_record(VkCommandBuffer buffer, uint32_t batch_count,
-                            const void *batches);
-
 typedef void tb_record_draw_batch(VkCommandBuffer buffer, uint32_t batch_count,
                                   const void *batches);
