8ade0c93a1cea627807870b3aee322e400b0d664
Honeybunch <amt3824@g.rit.edu>
2022-10-09 21:33:04 -0700

    Better water

--- src/ocean.hlsl
+++ src/ocean.hlsl
@@ -6,7 +6,7 @@
 // Contains the vertex stage
 #include "oceancommon.hlsli"
 
-float linear_depth(float near, float far, float depth)
+float linear_depth(float depth, float near, float far)
 {
   return near * far / (far + depth * (near - far));
 }
@@ -18,14 +18,6 @@ float depth_from_clip_z(float z, float near, float far)
 
 float4 frag(Interpolators i) : SV_TARGET
 {
-  // Sylized water
-  const float3 shallow = float3(0.0, 0.86, 0.79);
-  const float3 deep = float3(0.03, 0.08, 0.18);
-
-  float depth = 1.0 - pow(clamp((i.world_pos.y + 5.0) / 25.0, 0.0, 1.5), 1.2);
-  float opacity = lerp(0.1, 0.9, depth);
-  float3 base_color = lerp(shallow, deep, depth);
-
   float3 light_dir = normalize(float3(0.707, 0.707, 0));
 
   // Calculate normal after interpolation
@@ -33,9 +25,31 @@ float4 frag(Interpolators i) : SV_TARGET
   float3 V = normalize(camera_data.view_pos - i.world_pos);
   float NdotV = clamp(abs(dot(N, V)), 0.001, 1.0);
 
-  float3 color = float3(0.0f, 0.0f, 0.0f);
+  float3 base_color = float3(0, 0, 0);
+
+  // Underwater fog
+  {
+    // HACK: Need to paramaterize these
+    const float near = 0.1f;
+    const float far = 1000.0f;
+    const float fog_density = 0.15f;
+    const float3 fog_color = float3(0.305, 0.513, 0.662);
+
+    const float2 uv = i.screen_pos.xy / i.screen_pos.w;
+
+    float background_depth = linear_depth(depth_map.Sample(static_sampler, uv).r, near, far);
+    float surface_depth = depth_from_clip_z(i.screen_pos.z, near, far);
+
+    float depth_diff = background_depth - surface_depth;
+
+    float3 background_color = color_map.Sample(static_sampler, uv).rgb;
+    float fog = exp2(fog_density * depth_diff);
+
+    base_color = lerp(fog_color, background_color, fog);
+  }
 
   // PBR Lighting
+  float3 color = float3(0,0,0);
   {
     float metallic = 0.0;
     float roughness = 0.5;
@@ -78,5 +92,5 @@ float4 frag(Interpolators i) : SV_TARGET
   float3 ambient = float3(AMBIENT, AMBIENT, AMBIENT) * base_color;
   color += ambient;
 
-  return float4(color, opacity);
+  return float4(color, 1);
 }
