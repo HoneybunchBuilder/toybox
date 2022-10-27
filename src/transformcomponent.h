984c23028022abfdf8ea26055449fb9946031543
Honeybunch <amt3824@g.rit.edu>
2022-10-26 18:03:01 -0700

    Transforms have parents now

--- src/transformcomponent.h
+++ src/transformcomponent.h
@@ -5,7 +5,10 @@
 
 #define TransformComponentId 0xDEADBEEF
 
+typedef struct World World;
+
 typedef struct TransformComponentDescriptor {
+  World *world;
   EntityId parent;
   Transform transform;
 } TransformComponentDescriptor;
@@ -13,6 +16,7 @@ typedef struct TransformComponentDescriptor {
 typedef struct TransformComponent {
   bool dirty;
   float4x4 world_matrix;
+  ComponentStore *transform_store;
   Transform transform;
   EntityId parent;
 } TransformComponent;
