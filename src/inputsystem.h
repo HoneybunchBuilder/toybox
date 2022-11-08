a51f5adcc562e1ea68bf9564522c44496cfbe44f
Honeybunch <amt3824@g.rit.edu>
2022-11-07 17:54:00 -0800

    Doing some scaffolding to support game controllers

--- src/inputsystem.h
+++ src/inputsystem.h
@@ -6,15 +6,20 @@
 #define InputSystemId 0x8BADF00D
 
 typedef struct SDL_Window SDL_Window;
+typedef struct _SDL_GameController SDL_GameController;
 
 typedef struct InputSystemDescriptor {
   Allocator tmp_alloc;
   SDL_Window *window;
 } InputSystemDescriptor;
 
+#define TB_MAX_GAME_CONTROLLERS 4
+
 typedef struct InputSystem {
   Allocator tmp_alloc;
   SDL_Window *window;
+
+  SDL_GameController *controllers[TB_MAX_GAME_CONTROLLERS];
 } InputSystem;
 
 void tb_input_system_descriptor(SystemDescriptor *desc,
