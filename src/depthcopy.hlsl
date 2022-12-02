b6921831f6b06ad41f5295b1fb647f169d8a6de0
Honeybunch <amt3824@g.rit.edu>
2022-12-01 20:14:59 -0800

    Cleaning up

--- src/depthcopy.hlsl
+++ src/depthcopy.hlsl
@@ -1,9 +1,8 @@
 #include "fullscreenvert.hlsli"
 
-Texture2D depth_map : register(t0, space0); // Fragment Stage Only
+Texture2D depth_map : register(t0, space0);    // Fragment Stage Only
 sampler static_sampler : register(s1, space0); // Immutable Sampler
 
-float frag(Interpolators i) : SV_TARGET
-{
+float frag(Interpolators i) : SV_TARGET {
   return depth_map.Sample(static_sampler, i.uv0).r;
 }
--- src/imgui.hlsl
+++ src/imgui.hlsl
@@ -1,36 +1,31 @@
 #include "imgui.hlsli"
 
-[[vk::push_constant]]
-ConstantBuffer<ImGuiPushConstants> consts : register(b0);
+[[vk::push_constant]] ConstantBuffer<ImGuiPushConstants> consts : register(b0);
 
 Texture2D atlas : register(t0, space0);
 
 sampler static_sampler : register(s1, space0);
 
-struct VertexIn
-{
-    float2 position : SV_POSITION;
-    float2 uv : TEXCOORD0;
-    float4 color : COLOR0;
+struct VertexIn {
+  float2 position : SV_POSITION;
+  float2 uv : TEXCOORD0;
+  float4 color : COLOR0;
 };
 
-struct Interpolators
-{
-    float4 clip_pos : SV_POSITION;
-    float4 color : COLOR0;
-    float2 uv : TEXCOORD0;
+struct Interpolators {
+  float4 clip_pos : SV_POSITION;
+  float4 color : COLOR0;
+  float2 uv : TEXCOORD0;
 };
 
-Interpolators vert(VertexIn i)
-{
-    Interpolators o;
-    o.clip_pos = float4(i.position * consts.scale + consts.translation, 0.5, 1);
-    o.color = i.color;
-    o.uv = i.uv;
-    return o;
+Interpolators vert(VertexIn i) {
+  Interpolators o;
+  o.clip_pos = float4(i.position * consts.scale + consts.translation, 0.5, 1);
+  o.color = i.color;
+  o.uv = i.uv;
+  return o;
 }
 
-float4 frag(Interpolators i) : SV_TARGET
-{
-    return i.color * atlas.Sample(static_sampler, i.uv);
+float4 frag(Interpolators i) : SV_TARGET {
+  return i.color * atlas.Sample(static_sampler, i.uv);
 }
