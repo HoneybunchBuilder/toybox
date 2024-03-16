38b6a0cb6f801938f8a549678276685533a0b123
Honeybunch <amt3824@g.rit.edu>
2024-03-16 11:58:51 -0700

    Adding a small thread safe queue

---
+++ include/tbqueue.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "dynarray.h"
+#include <SDL3/SDL_Mutex.h>
+
+#define TB_QUEUE_OF(type)                                                      \
+  struct {                                                                     \
+    TB_DYN_ARR_OF(type) storage;                                               \
+    SDL_Mutex *mutex;                                                          \
+  }
+
+#define TB_QUEUE_RESET(queue, allocator, cap)                                  \
+  {                                                                            \
+    TB_DYN_ARR_RESET((queue).storage, (allocator), (cap));                     \
+    if (queue.mutex == NULL) {                                                 \
+      queue.mutex = SDL_CreateMutex();                                         \
+    }                                                                          \
+  }
+
+#define TB_QUEUE_DESTROY(queue)                                                \
+  {                                                                            \
+    TB_DYN_ARR_DESTROY(queue.storage);                                         \
+    SDL_DestroyMutex(queue.mutex);                                             \
+  }
+
+#define TB_QUEUE_PUSH(queue, element)                                          \
+  if (SDL_TryLockMutex(queue.mutex) == 0) {                                    \
+    TB_DYN_ARR_APPEND(queue.storage, element);                                 \
+    SDL_UnlockMutex(queue.mutex);                                              \
+  }
+
+#define TB_QUEUE_POP(queue, out)                                               \
+  _Pragma("clang diagnostic push");                                            \
+  _Pragma("clang diagnostic ignored \"-Wgnu-statement-expression\"");          \
+  ({                                                                           \
+    if (TB_DYN_ARR_SIZE(queue.storage) == 0) {                                 \
+      false;                                                                   \
+    }                                                                          \
+    if (SDL_TryLockMutex(queue.mutex) == 0) {                                  \
+      (*out) = *TB_DYN_ARR_BACKPTR(queue.storage);                             \
+      TB_DYN_ARR_POP(queue.storage);                                           \
+      true;                                                                    \
+    } else {                                                                   \
+      false;                                                                   \
+    }                                                                          \
+  }) _Pragma("clang diagnostic pop");
+
+#define TB_QUEUE_CLEAR(queue)                                                  \
+  while (TB_QUEUE_POP(queue)) {                                                \
+  }
