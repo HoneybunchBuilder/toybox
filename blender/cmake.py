8bed0d64278e704517f8a6ecf56ca9d6fdfbbb85
Honeybunch <amt3824@g.rit.edu>
2024-09-21 15:43:45 -0700

    Updating plugin a bit

--- blender/cmake.py
+++ blender/cmake.py
@@ -1,51 +1,55 @@
 import bpy
 import os
+import json
 from pathlib import Path
 import subprocess
 import threading
 
+
 def get_out_dir(context):
     abs_path = os.path.abspath(context.scene.toybox.project_path)
-    config = Path(context.scene.toybox.build_config)
-    preset = context.scene.toybox.build_preset
-    preset_opts = preset.split('-')
-    arch = Path(preset_opts[0])
-    plat = ''
-    if 'windows-ninja' in preset:
-        plat = 'windows'
-    elif 'windows-static-ninja' in preset:
-        plat = 'windows-static'
-    elif 'windows-vs2022' in preset:
-        plat = 'windows-clangcl'
-    elif 'windows-static-vs2022' in preset:
-        plat = 'windows-static-clangcl'
-    plat = Path(plat)
-    
-    return abs_path / Path('build') / arch  / plat 
-  
+    preset_name = context.scene.toybox.build_preset
+
+    # Look up build dir from preset
+    json_path = os.path.join(abs_path, os.path.join("CMakePresets.json"))
+    if not os.path.exists(json_path):
+        return Path("")
+
+    with open(json_path, "r") as presets_json:
+        presets = json.load(presets_json)
+        if preset_name in presets:
+            preset = presets[preset_name]
+            if "binaryDir" in preset:
+                binary_dir = preset["binaryDir"]
+                resolved_dir = binary_dir.format(sourceDir=abs_path)
+                return abs_path / Path(resolved_dir)
+
+    return Path("")
+
+
 def get_exe(context, out_dir):
-  exe = context.scene.toybox.project_name + '.exe'
-  
-  for dirpath, dirnames, filenames in os.walk(out_dir):
-    for filename in [f for f in filenames if f == exe]:
-      return Path(dirpath) / Path(filename)
-  
-  return None
+    exe = context.scene.toybox.project_name + ".exe"
+
+    for dirpath, dirnames, filenames in os.walk(out_dir):
+        for filename in [f for f in filenames if f == exe]:
+            return Path(dirpath) / Path(filename)
+
+    return None
+
 
 def run_build(context):
     abs_path = os.path.abspath(context.scene.toybox.project_path)
     config = context.scene.toybox.build_config
     preset = context.scene.toybox.build_preset
 
-    subprocess.run('cmake --preset ' + preset, cwd=abs_path)
-    subprocess.run(
-        'cmake --build --preset ' + config + '-' + preset, cwd=abs_path)
+    subprocess.run("cmake --preset " + preset, cwd=abs_path)
+    subprocess.run("cmake --build --preset " + config + "-" + preset, cwd=abs_path)
 
 
 class BuildOperator(bpy.types.Operator):
-    bl_idname = 'tb.build'
-    bl_label = 'Build'
-    bl_options = {'INTERNAL'}
+    bl_idname = "tb.build"
+    bl_label = "Build"
+    bl_options = {"INTERNAL"}
 
     def execute(self, context):
         context.window_manager.modal_handler_add(self)
@@ -53,23 +57,23 @@ class BuildOperator(bpy.types.Operator):
         self.thread = threading.Thread(target=run_build, args=(context,))
         self.thread.start()
 
-        return {'RUNNING_MODAL'}
+        return {"RUNNING_MODAL"}
 
     def modal(self, context, event):
         if self.thread.is_alive():
-            return {'PASS_THROUGH'}
+            return {"PASS_THROUGH"}
 
-        self.report({'INFO'}, 'Build Complete')
-        return {'FINISHED'}
+        self.report({"INFO"}, "Build Complete")
+        return {"FINISHED"}
 
 
 class RunOperator(bpy.types.Operator):
-    bl_idname = 'tb.run'
-    bl_label = 'Run'
+    bl_idname = "tb.run"
+    bl_label = "Run"
 
     def execute(self, context):
         output_dir = get_out_dir(context)
         exe = get_exe(context, output_dir)
 
         subprocess.Popen(exe, cwd=output_dir)
-        return {'FINISHED'}
+        return {"FINISHED"}
--- blender/mainpanel.py
+++ blender/mainpanel.py
@@ -4,55 +4,71 @@ import sys
 
 import bpy
 
+
+def check_preset_platform(host, presets, preset):
+    # See if this preset doesn't match the host platform
+    if "condition" in preset:
+        condition = preset["condition"]
+        if "type" in condition:
+            if condition["type"] == "equals":
+                if condition["rhs"] != host:
+                    return False
+    # Check all inherited presets too
+    if "inherits" in preset:
+        for parent_name in preset["inherits"]:
+            # Find preset by name
+            for parent in presets:
+                if parent["name"] == parent_name:
+                    if not check_preset_platform(host, presets, parent):
+                        return False
+    return True
+
+
 def parse_presets(self, context):
     abs_path = os.path.abspath(context.scene.toybox.project_path)
-    json_path = os.path.join(abs_path, os.path.join('CMakePresets.json'))
+    json_path = os.path.join(abs_path, os.path.join("CMakePresets.json"))
     if not os.path.exists(json_path):
         return []
 
-    host_platform = 'Windows'
+    host = "Windows"
 
-    with open(json_path, 'r') as presets_json:
+    with open(json_path, "r") as presets_json:
         presets = json.load(presets_json)
 
         editor_presets = []
 
-        configure_presets = presets['configurePresets']
+        configure_presets = presets["configurePresets"]
         for preset in configure_presets:
-            if 'condition' in preset:
-                condition = preset['condition']
-                if 'type' in condition:
-                    # Taking a short cut here...
-                    if condition['type'] == 'equals':
-                        if condition['rhs'] == host_platform:
-                            editor_presets.append(
-                                (preset['name'], preset['displayName'], ''))
-                else:
-                    editor_presets.append(
-                        (preset['name'], preset['displayName'], ''))
+            if "hidden" in preset:
+                if preset["hidden"]:
+                    continue
+            if check_preset_platform(host, configure_presets, preset):
+                editor_presets.append((preset["name"], preset["displayName"], ""))
         return editor_presets
 
 
 class ToyboxSettings(bpy.types.PropertyGroup):
-    project_path: bpy.props.StringProperty(name='Path', default='../')
-    project_name: bpy.props.StringProperty(
-        name='Name', default='toybox-game')
-    editor: bpy.props.StringProperty(name='Editor', default='code')
-    build_preset: bpy.props.EnumProperty(name='Preset', items=parse_presets)
-    build_config: bpy.props.EnumProperty(name='Config', items=[
-        ('debug', 'Debug', ''),
-        ('relwithdebinfo', 'RelWithDebInfo', ''),
-        ('release', 'Release', ''),
-    ] 
+    project_path: bpy.props.StringProperty(name="Path", default="../")
+    project_name: bpy.props.StringProperty(name="Name", default="toybox-game")
+    editor: bpy.props.StringProperty(name="Editor", default="code")
+    build_preset: bpy.props.EnumProperty(name="Preset", items=parse_presets)
+    build_config: bpy.props.EnumProperty(
+        name="Config",
+        items=[
+            ("debug", "Debug", ""),
+            ("relwithdebinfo", "RelWithDebInfo", ""),
+            ("release", "Release", ""),
+        ],
     )
 
+
 class OBJECT_PT_Toybox(bpy.types.Panel):
-    bl_label = 'Toybox'
-    bl_idname = 'OBJECT_PT_Toybox'
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = 'Toybox'
-    bl_context = 'objectmode'
+    bl_label = "Toybox"
+    bl_idname = "OBJECT_PT_Toybox"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Toybox"
+    bl_context = "objectmode"
 
     @classmethod
     def poll(self, context):
@@ -62,21 +78,22 @@ class OBJECT_PT_Toybox(bpy.types.Panel):
         layout = self.layout
 
         row = layout.row()
-        row.prop(context.scene.toybox, 'project_path')
-        row.prop(context.scene.toybox, 'project_name')
+        row.prop(context.scene.toybox, "project_path")
+        row.prop(context.scene.toybox, "project_name")
 
         row = layout.row()
-        row.prop(context.scene.toybox, 'build_preset')
-        row.prop(context.scene.toybox, 'build_config')
+        row.prop(context.scene.toybox, "build_preset")
+        row.prop(context.scene.toybox, "build_config")
 
         row = layout.row()
-        row.operator('tb.build')
-        row.operator('tb.run')
-        row.operator('tb.refresh_components')
+        row.operator("tb.build")
+        row.operator("tb.run")
+        row.operator("tb.refresh_components")
 
         row = layout.row()
-        row.prop(context.scene.toybox, 'editor')
-        row.operator('tb.open_editor')
+        row.prop(context.scene.toybox, "editor")
+        row.operator("tb.open_editor")
+
 
 def register():
     bpy.types.Scene.toybox = bpy.props.PointerProperty(type=ToyboxSettings)
