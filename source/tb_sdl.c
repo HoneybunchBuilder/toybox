089955a8978cbd7850c257308b3d3ac8b048c8b0
Honeybunch <amt3824@g.rit.edu>
2024-11-23 19:59:41 -0800

    Fixing failure to mmap on linux

--- source/tb_sdl.c
+++ source/tb_sdl.c
@@ -12,7 +12,8 @@ void *tb_io_mmap(SDL_IOStream *file, size_t size) {
   const char *prop_name = SDL_PROP_IOSTREAM_STDIO_FILE_POINTER;
 #endif
   void *file_handle = SDL_GetPointerProperty(props, prop_name, NULL);
-  return tb_mmap(0, size, 0, 0, file_handle, 0);
+  int32_t file_descriptor = fileno(file_handle);
+  return tb_mmap(0, size, PROT_READ, MAP_SHARED, file_descriptor, 0);
 }
 
 void tb_io_munmap(void *data, size_t size) { tb_munmap(data, size); }
